138d8121b8980a4a0eac1d8f0023bfab79f21cac
Ashley Finafrock <v-asho@microsoft.com>
2021-04-24 17:02:18 -0700

    Added Navbar and Input (for form) components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import "./App.css";
 import { Component } from "react";
-import Movies from "./components/movies";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeart as unfilledHeart } from "@fortawesome/free-regular-svg-icons";
 import {
   faHeart as solidHeart,
   faSortDown,
   faSortUp,
 } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as unfilledHeart } from "@fortawesome/free-regular-svg-icons";
+import NavBar from "./components/common/navBar";
+import Movies from "./components/movies";
+import NotFound from "./components/notFound";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import "./App.css";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
       </main>
     );
   }
