b9d61e404773b7c76fe2863a22844c721b9c14be
Ashley Finafrock <v-asho@microsoft.com>
2021-05-06 20:59:41 -0700

    Create ProtectedRoute component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import {
   faSortUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer } from "react-toastify";
-import NavBar from "./components/common/navBar";
-import Movies from "./components/movies";
-import NotFound from "./components/notFound";
-import Rentals from "./components/rentals";
 import Customers from "./components/customers";
-import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import Logout from "./components/logout";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import NavBar from "./components/common/navBar";
+import NotFound from "./components/notFound";
+import ProtectedRoute from "./components/common/protectedRoute";
 import RegistrationForm from "./components/registrationForm";
+import Rentals from "./components/rentals";
 import auth from "./services/authService";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -40,13 +41,7 @@ class App extends Component {
             <Route path="/register" component={RegistrationForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route
-              path="/movies/:id"
-              render={(props) => {
-                if (!user) return <Redirect to="/login" />;
-                return <MovieForm {...props} />;
-              }}
-            />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
             <Route
               path="/movies"
               render={(props) => <Movies {...props} user={this.state.user} />}
